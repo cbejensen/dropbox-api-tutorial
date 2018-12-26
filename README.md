3acfca52522f6c1358b138da0e652cb8510034a8
Christian Jensen <cbejensen@gmail.com>
2018-12-26 08:52:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dropbox API Tutorial
+
+Within the `lessons` folder you'll find lessons on how to use Dropbox API. To view a lesson, enter `npm run {lessonNum}` in your terminal, where `{lessonNum}` is the lesson you want to view.
+
+## What do I need to take this tutorial?
+
+Some knowledge of JavaScript. We'll quickly address some of the ES6+ features we use as we go through.
---
+++ lessons/1. Importing the SDK/index.js
@@ -0,0 +1,11 @@
+import { Dropbox } from 'dropbox'
+// note that Dropbox has to be a named import
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#Import_a_single_export_from_a_module
+
+const dbx = new Dropbox({
+  accessToken:
+    'aeOL1E1HS0AAAAAAAAAAGy2WjtZlAq0zP8WS1HYNvMmP__7ii2-2vMZ-8RXd9fJA',
+  fetch // could be another fetch library
+})
+
+console.log(dbx)

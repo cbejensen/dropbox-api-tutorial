46e627309881bca32f009763d3d223edb4484060
Christian Jensen <cbejensen@gmail.com>
2018-12-26 09:10:40 -0700

    added lesson 3

--- lessons/2. Getting A List of Files/index.js
+++ lessons/2. Getting A List of Files/index.js
@@ -6,46 +6,12 @@ const dbx = new Dropbox({
   fetch
 })
 
-let files = []
-let cursor = ''
-
-const filesListElem = document.querySelector('.js-files')
-const getFilesBtn = document.querySelector('.js-get-files')
-getFilesBtn.addEventListener('click', e => getMoreFiles())
-
-const getMoreFiles = () =>
-  dbx
-    .filesListFolderContinue({ cursor })
-    .then(res => {
-      updateFiles(res)
-      renderFiles()
-      if (!res.has_more) {
-        getFilesBtn.classList.add('hidden')
-      }
-    })
-    .catch(err => console.error(err))
-
-const updateFiles = dbxRes => {
-  files = [...files, ...dbxRes.entries]
-  cursor = dbxRes.cursor
-}
-
-const renderFiles = () => {
-  filesListElem.innerHTML = files.reduce((prevFiles, currentFile) => {
-    return `${prevFiles}<li class="file">${currentFile.name}</li>`
-  }, ``)
-}
-
 dbx
   .filesListFolder({
-    path: '/Apps/Expense Organizer Demo',
-    limit: 8
+    path: '/Apps/Expense Organizer Demo'
   })
   .then(res => {
-    updateFiles(res)
-    renderFiles()
-    if (res.has_more) {
-      getFilesBtn.classList.remove('hidden')
-    }
+    console.log(res)
+    console.table(res.entries)
   })
   .catch(err => console.error(err))
